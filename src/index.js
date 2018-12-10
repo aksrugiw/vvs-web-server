178a62610d38772187c8838bceeefa34ec9cebd6
Kinga Wigurska-Bąk <kwigurskabak@gmail.com>
2018-12-11 00:29:05 +0100

    Fix port for heroku

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const http = require('http');
+var port = process.env.PORT || 8080;
+
 const app = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write('Welcome in very very simple server!');
     res.end();
 });
 
-app.listen(3000);
+app.listen(port);
